756778475a5d5b8c4ec46d388cc0c5a809f7350f
sujin <tnwlssl0729@naver.com>
2024-09-27 23:30:55 +0900

    [MUI 적용] ButtonSection 컴포넌트

---
+++ src/components/ButtonSection.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "../style/ButtonSection.module.scss";
+import FileDownloadOutlinedIcon from "@mui/icons-material/FileDownloadOutlined";
+import ContentPasteOutlinedIcon from "@mui/icons-material/ContentPasteOutlined";
+
+const ButtonSection = () => {
+  return (
+    <div className={styles.buttonContainer}>
+      <button className={styles.button}>
+        <div className={styles.iconTextContainer}>
+          <FileDownloadOutlinedIcon style={{ marginRight: "5px" }} />
+          Save as Markdown
+        </div>
+      </button>
+      <button className={styles.button}>
+        <div className={styles.iconTextContainer}>
+          <ContentPasteOutlinedIcon style={{ marginRight: "5px" }} />
+          Copy Markdown
+        </div>
+      </button>
+    </div>
+  );
+};
+
+export default ButtonSection;

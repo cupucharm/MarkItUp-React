4392eb0de99cc9ba0efd9c5a8480b6d70e091e3b
sujin <tnwlssl0729@naver.com>
2025-01-04 00:08:24 +0900

    로그인 로직 수정

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,31 @@
 import React, { useState } from "react";
 import styles from "../style/Login.module.scss";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
   const [userId, setUserId] = useState("");
   const [userPwd, setUserPwd] = useState("");
   const [error, setError] = useState("");
 
+  const navigate = useNavigate();
+
+  const loginSuccess = () => {
+    window.scrollTo(0, 0);
+    navigate("/");
+  };
+
   const handleLogin = async () => {
+    // 아이디와 비밀번호가 입력되었는지 확인
+    if (!userId) {
+      setError("아이디를 입력해 주세요.");
+      return;
+    }
+
+    if (!userPwd) {
+      setError("비밀번호를 입력해 주세요.");
+      return;
+    }
+
     try {
       const response = await fetch("/sooz/login", {
         method: "POST",
@@ -17,11 +36,19 @@ function Login() {
       });
 
       if (!response.ok) {
-        throw new Error("로그인 실패");
+        throw new Error("로그인 실패 : ", response);
       }
 
       const data = await response.json();
-      console.log("로그인 성공:", data);
+      if (data.status === true) {
+        console.log("로그인 성공:", data);
+        setError("");
+        loginSuccess();
+      } else {
+        console.log("로그인 실패:", data);
+        setError(data.message);
+      }
+
       // 로그인 성공 후 처리
     } catch (error) {
       setError(error.message);
@@ -35,36 +62,38 @@ function Login() {
 
   return (
     <div className={styles.login}>
-      <h1 style={{ textAlign: "center" }}>로그인</h1>
-
-      <form onSubmit={handleSubmit}>
-        <div>
+      <form className={styles.loginForm} onSubmit={handleSubmit}>
+        <div className={styles.loginBox}>
           <div>
-            <label htmlFor="userId">아이디</label>
             <input
               type="text"
               id="userId"
+              placeholder="아이디"
               value={userId}
               onChange={(e) => setUserId(e.target.value)}
-              required
+              // required
+              className={styles.inputField}
             />
           </div>
 
           <div>
-            <label htmlFor="userPwd">비밀번호</label>
             <input
               type="password"
               id="userPwd"
+              placeholder="비밀번호"
               value={userPwd}
               onChange={(e) => setUserPwd(e.target.value)}
-              required
+              // required
+              className={styles.inputField}
             />
           </div>
-          <button type="submit">로그인</button>
         </div>
-      </form>
+        {error && <p style={{ color: "red", fontSize: "0.8em" }}>{error}</p>}
 
-      {error && <p style={{ color: "red" }}>{error}</p>}
+        <button className={styles.loginButton} type="submit">
+          로그인
+        </button>
+      </form>
     </div>
   );
 }

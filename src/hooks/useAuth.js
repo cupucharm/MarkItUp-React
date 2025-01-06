c31077629fa573881b3766110e273dd1a36ddd6c
sujin <tnwlssl0729@naver.com>
2025-01-07 07:00:54 +0900

    로그인 권한 관리

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export const useAuth = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLoginSuccess = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = async () => {
+    try {
+      const response = await fetch("/sooz/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("로그아웃 실패");
+      }
+
+      const data = await response.json();
+      if (data.status === true) {
+        console.log("로그아웃 성공:", data);
+        setIsLoggedIn(false);
+      } else {
+        console.log("로그아웃 실패:", data);
+        alert(data.message);
+      }
+    } catch (error) {
+      console.error("로그아웃 중 오류 발생:", error);
+      alert("로그아웃 중 오류가 발생했습니다.");
+    }
+  };
+
+  return { isLoggedIn, setIsLoggedIn, handleLoginSuccess, handleLogout };
+};

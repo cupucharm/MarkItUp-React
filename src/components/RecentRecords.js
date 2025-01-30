e50af1bef3a24d9492192f64016e5ab35d6542d8
sujin <tnwlssl0729@naver.com>
2025-01-30 14:49:31 +0900

    최근기록 로그인여부 확인 추가

--- src/components/RecentRecords.js
+++ src/components/RecentRecords.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import styles from "../style/RecentRecords.module.scss";
 import headerStyles from "../style/Header.module.scss";
 
-const RecentRecords = ({ isOpen }) => {
+const RecentRecords = ({ isOpen, isLoggedIn }) => {
   const [headerHeight, setHeaderHeight] = useState(0);
 
   useEffect(() => {
@@ -18,7 +18,11 @@ const RecentRecords = ({ isOpen }) => {
       style={{ top: headerHeight }}
     >
       <h2 style={{ paddingLeft: "3vh" }}>최근 기록</h2>
-      <p style={{ textAlign: "center" }}>로그인이 필요합니다.</p>
+      {isLoggedIn ? (
+        <p>로그인 성공 !</p>
+      ) : (
+        <p style={{ textAlign: "center" }}>로그인이 필요합니다.</p>
+      )}
     </div>
   );
 };
